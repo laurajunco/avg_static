31692e740dd11bb8df26c37cccf921ace5a503b0
Laura Junco <lc.junco10@uniandes.edu.co>
2021-07-02 13:36:58 -0500

    Transitions y og

--- wp-content/themes/aquivivegente/js/transitions.js
+++ wp-content/themes/aquivivegente/js/transitions.js
@@ -86,6 +86,7 @@
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     
     window.addEventListener('resize', () => {
+      hideElems()
       let vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
